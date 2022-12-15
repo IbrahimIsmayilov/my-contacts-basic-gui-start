0cd1c4dd00cf296aba4bb6d5bf6e9fb05baa6257
IbrahimIsmayilov <90726322+IbrahimIsmayilov@users.noreply.github.com>
2022-12-15 14:46:16 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -35,28 +35,32 @@ function displaycontactsArray() {
 }
 
 function addContact() {
-  let contactName = prompt("Enter New Contact Name:");
   let contactEmail = prompt("Enter New Contact Email:");
+  let index = findByEmail(contactEmail);
+  if (index !== -1) {
+  let contactName = prompt("Enter New Contact Name:");
   let contactNumber = prompt("Enter New Contact Number:");
   let contactCountry = prompt("Enter New Contact Country");
   contactsArray.push(newContact(contactName, contactEmail, contactNumber, contactCountry));
-
   outputEl.innerHTML = `Task Added: ${contactName}`;
   alert("New Contact has been added");
+  console.log("Contact with that email could not be found");
   displayAll();
   saveContact();
-
+  } else {
+    alert(`Contact with that email was found at position ${index}`);
+  } 
 }
 
 function removeContact() {
-  let index = +prompt("Enter the contact # to remove:");
-  if (index >= 0 && index < contactsArray.length) {
+  let askEmail = prompt("Enter the email you want to remove:");
+  let index = findByEmail(askEmail);
+  if (index !== -1) {
     contactsArray.splice(index, 1);
-    alert(`Contact # ${index} has been removed`);
     displayAll();
     saveContact();
   } else {
-    alert("Invalid Contact #");
+    alert("Invalid Email #");
   }
 }
 
@@ -97,16 +101,10 @@ function displayByCountry() {
 function findByEmail(emailInput) {
   for (let i = 0; i < contactsArray.length; i++) {
     if (emailInput === contactsArray[i].contactEmail) {
-      divStr += `
-      <div style='border: 1px solid grey'>
-      <h1> ${contactsArray[i].contactName} </h1>
-      <p> ${contactsArray[i].contactEmail} </p>
-      <p> ${contactsArray[i].contactNumber} (${contactsArray[i].contactCountry})</p>
-      </div>
-      `
-     } else {
-     }
-    }
+      return i;
+     } 
+  }
+  return -1;
   }
 
 
